17da3a39fb3a1c53adc69d87abe7b6d8d03a5b56
Akash-Behara <behara.akash68@gmail.com>
2024-10-30 12:57:27 +0530

    setup privy login

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -6,10 +6,12 @@ import React from "react";
 const Signin = () => {
   const { ready, authenticated, login, getAccessToken } = usePrivy();
 
+  console.log('getAccessToken', getAccessToken());
+
   return (
     <div>
       <div>Signin Farcaster</div>
-      {/* <button onClick={login}>Sign in</button> */}
+      <button onClick={login}>Sign in</button>
     </div>
   );
 };
