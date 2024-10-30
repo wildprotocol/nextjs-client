998cbc5174dc132dcfeec00646eacc60678cfcb9
Akash-Behara <behara.akash68@gmail.com>
2024-10-30 12:24:34 +0530

    privy

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,18 +1,17 @@
-'use client';
+"use client";
 
-import { usePrivy } from '@privy-io/react-auth';
-import React from 'react'
+import { usePrivy } from "@privy-io/react-auth";
+import React from "react";
 
 const Signin = () => {
-    const { ready, authenticated, login, getAccessToken } = usePrivy();
+  const { ready, authenticated, login, getAccessToken } = usePrivy();
 
+  return (
+    <div>
+      <div>Signin Farcaster</div>
+      {/* <button onClick={login}>Sign in</button> */}
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <div>Signin Farcaster</div>
-            {/* <button onClick={login}>Sign in</button> */}
-        </div>
-    )
-}
-
-export default Signin
+export default Signin;
